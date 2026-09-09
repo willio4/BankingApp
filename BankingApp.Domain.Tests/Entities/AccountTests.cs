3a6fb9d1aaf0b8abbf74537484b6454cf6a04a9e
Omar Williams <willio4@unlv.nevada.edu>
2026-09-09 00:58:36 -0700

    Add unit tests for Account balance calculation
    Implements tests to verify that new accounts start with a zero balance and that adding ledger entries correctly affects the calculated balance.

--- BankingApp.Domain.Tests/Entities/AccountTests.cs
+++ BankingApp.Domain.Tests/Entities/AccountTests.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BankingApp.Domain.Entities;
+using BankingApp.Domain.ValueObjects;
 using Xunit;
 
 namespace BankingApp.Domain.Tests.Entities
@@ -9,9 +11,34 @@ namespace BankingApp.Domain.Tests.Entities
     public class AccountTests
     {
         [Fact]
-        public void Test1()
+        public void AccountCreateSuccessful_ZeroInitialBalance()
         {
-            Assert.True(true);
+            Money init = new(500);
+            string accountNumber = Random.Shared.NextInt64(100000000000, 1000000000000).ToString();
+            Account myAccount = new(accountNumber,Enums.AccountType.Checking, "USD");
+
+            Assert.Equal(0, myAccount.CalculateBalance());
         }
+
+
+        [Fact]
+        public void AccountCreateSuccessful_CalculateBalance()
+        {
+            // Given
+            string accountNumber = Random.Shared.NextInt64(100000000000, 1000000000000).ToString();
+            Account myAccount = new(accountNumber, Enums.AccountType.Checking);
+            Money deposit = new(100);
+            LedgerEntry deposit100 = new(myAccount.Id, deposit, Enums.EntryType.Credit, DateTime.Now);
+            Money withdraw = new(30);
+            LedgerEntry withdraw30 = new(myAccount.Id, withdraw, Enums.EntryType.Debit, DateTime.Now);
+            // When
+            myAccount.AddLedgerEntry(deposit100);
+            myAccount.AddLedgerEntry(withdraw30);
+            decimal expected = (deposit - withdraw).Amount;
+            // Then
+            Assert.Equal(expected, myAccount.CalculateBalance());
+        }
+
+        
     }
 }
