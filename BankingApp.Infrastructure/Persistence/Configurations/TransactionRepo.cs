44bb1400fb43447f7f6f0a5c9ee2bf0966c3f9a4
Omar Williams <willio4@unlv.nevada.edu>
2026-09-10 12:43:56 -0700

    Initialize persistence repositories
    Adds skeleton classes for Account, Customer, and Transaction repositories in the Infrastructure layer.

---
+++ BankingApp.Infrastructure/Persistence/Configurations/TransactionRepo.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankingApp.Infrastructure.Persistence.Configurations
+{
+    public class TransactionRepo
+    {
+        
+    }
+}
