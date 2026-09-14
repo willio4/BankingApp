b7e786969e5774294fe981c3385db32d32d8f3a7
Omar Williams <willio4@unlv.nevada.edu>
2026-09-13 20:36:50 -0700

    Configure dependency injection and MVC middleware
    Register application services and repositories in the DI container and enable controller routing and authorization in the web application pipeline.

--- BankingApp.UI/Program.cs
+++ BankingApp.UI/Program.cs
@@ -1,4 +1,8 @@
+using BankingApp.Application.Common.Interfaces.Repositories;
+using BankingApp.Application.Common.Interfaces.Services;
+using BankingApp.Application.Services;
 using BankingApp.Infrastructure.Persistence;
+using BankingApp.Infrastructure.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,4 +12,18 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
 });
 
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+builder.Services.AddScoped<ICustomerRepository, CustomerRepo>();
+builder.Services.AddScoped<ITransactionRepository, TransactionRepo>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IAccountService, AccountService>();
+builder.Services.AddScoped<ITransactionService, TransactionService>();
+
+
+
 var app = builder.Build();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
