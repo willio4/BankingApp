2e84f456704badd9e863f4ef5e460d6cf71f5ab5
Omar Williams <willio4@unlv.nevada.edu>
2026-09-19 13:50:56 -0700

    Refactor CustomerController to enhance GetCustomerById, AddCustomer, and UpdateCustomer methods with proper DTO handling and cancellation support

--- BankingApp.WebAPI/Controllers/CustomerController.cs
+++ BankingApp.WebAPI/Controllers/CustomerController.cs
@@ -1,10 +1,14 @@
+using System.Net;
+using BankingApp.Application.Common.Interfaces.Repositories;
 using BankingApp.Application.Common.Interfaces.Services;
 using BankingApp.Application.DTOs;
 using BankingApp.Domain.Entities;
 using BankingApp.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BankingApp.WebAPI.Controllers
 {
@@ -30,18 +34,54 @@ namespace BankingApp.WebAPI.Controllers
         }
 
         [HttpGet("{customerId:guid}")]
-        public async Task<ActionResult<Customer>> GetCustomerById(Guid customerId)
+        public async Task<ActionResult<CustomerDTO>> GetCustomerById(Guid customerId)
         {
             if(_context.Customers == null) return NotFound();
 
             Customer? customer = await _context.Customers.FindAsync(customerId);
+
+            CancellationTokenSource cts = new();
 
             if(customer == null)
             {
                 return Problem("Customer doesn't exist", statusCode: 404, title: "Customer Search");
             }
 
-            return customer;
+            CustomerDTO? customerDTO = await _customerService.GetCustomerByIdAsync(customer.Id, cts.Token);
+        
+            if(customerDTO is null) {
+                return Problem("Customer doesn't exist", statusCode: 404, title: "Customer Search");
+            }
+
+            return customerDTO;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CustomerDTO>> AddCustomer([FromBody]CreateCustomerRequestDTO customerRequestDTO)
+        {
+            CancellationTokenSource cts = new();
+            CustomerDTO customerDTO = await _customerService.CreateCustomerAsync(customerRequestDTO, cts.Token);
+
+            if(ModelState.IsValid)
+            {
+                return CreatedAtAction(nameof(GetCustomerById), new { customerId = customerDTO.Id }, customerDTO);
+            }
+            else
+            {
+
+                return Problem("Customer could not be created");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<CustomerDTO>> UpdateCustomer(CreateCustomerRequestDTO updated, Guid id)
+        {
+            CancellationTokenSource cts = new();
+            CustomerDTO? old = await _customerService.GetCustomerByIdAsync(id, cts.Token);
+            if(old is null) return Problem("Customer does not exist");
+            CustomerDTO? current = await _customerService.UpdateCustomer(old, updated, cts.Token);
+
+            return Ok(current);
         }
     }
 }
