35cd7a61f6a665f3870bcdc30041b61a5c78c116
Omar Williams <willio4@unlv.nevada.edu>
2026-09-20 15:44:19 -0700

    Configure EF Core mapping for status columns and relationships
    Updates entity configurations to handle the new status properties and properly maps the one-to-many relationship between customers and accounts.

--- BankingApp.Infrastructure/Persistence/Configurations/AccountConfiguration.cs
+++ BankingApp.Infrastructure/Persistence/Configurations/AccountConfiguration.cs
@@ -16,6 +16,9 @@ namespace BankingApp.Infrastructure.Persistence.Configurations
             builder.Property(a => a.AccountNumber)
                 .IsRequired()
                 .HasMaxLength(12);
+            builder.Property(a => a.AccountStatus)
+                .IsRequired()
+                .HasDefaultValue(Domain.Enums.AccountStatus.Active);
             builder.HasIndex(a => a.AccountNumber)
                 .IsUnique();
             builder.HasMany(a => a.LedgerEntries)
@@ -25,21 +28,21 @@ namespace BankingApp.Infrastructure.Persistence.Configurations
             builder.Navigation(a => a.LedgerEntries)
                 .UsePropertyAccessMode(PropertyAccessMode.Field);
             builder.HasData(
-                new Account(Guid.Parse("81c8c2f1-1444-42ae-a6e5-272886a99f7e"), "693762756416", Guid.Parse("7222f4a6-b8ca-4cc5-ad74-7b4b34d79779"), Domain.Enums.AccountType.Checking, "USD"),
+                new Account(Guid.Parse("81c8c2f1-1444-42ae-a6e5-272886a99f7e"), "693762756416", Guid.Parse("7222f4a6-b8ca-4cc5-ad74-7b4b34d79779"), Domain.Enums.AccountType.Checking, "USD", Domain.Enums.AccountStatus.Active),
 
-                new Account(Guid.Parse("c7e97394-c25a-4f38-a54e-82e21610066d"), "990888064302", Guid.Parse("7222f4a6-b8ca-4cc5-ad74-7b4b34d79779"), Domain.Enums.AccountType.Savings, "USD"),
+                new Account(Guid.Parse("c7e97394-c25a-4f38-a54e-82e21610066d"), "990888064302", Guid.Parse("7222f4a6-b8ca-4cc5-ad74-7b4b34d79779"), Domain.Enums.AccountType.Savings, "USD", Domain.Enums.AccountStatus.Active),
 
-                new Account(Guid.Parse("4cb0b54d-3874-464d-8acb-b3242012d81f"), "660279434684", Guid.Parse("4bf2a680-ac31-4206-be1a-8effc3d6c427"), Domain.Enums.AccountType.Checking, "USD"),
+                new Account(Guid.Parse("4cb0b54d-3874-464d-8acb-b3242012d81f"), "660279434684", Guid.Parse("4bf2a680-ac31-4206-be1a-8effc3d6c427"), Domain.Enums.AccountType.Checking, "USD", Domain.Enums.AccountStatus.Active),
                 
-                new Account(Guid.Parse("a0901605-7462-45b0-9f0c-06b58bb724e6"), "752142281253", Guid.Parse("4bf2a680-ac31-4206-be1a-8effc3d6c427"), Domain.Enums.AccountType.Savings, "USD"),
+                new Account(Guid.Parse("a0901605-7462-45b0-9f0c-06b58bb724e6"), "752142281253", Guid.Parse("4bf2a680-ac31-4206-be1a-8effc3d6c427"), Domain.Enums.AccountType.Savings, "USD", Domain.Enums.AccountStatus.Active),
                 
-                new Account(Guid.Parse("ee722d5c-fa75-49ff-a438-ae2ea86b2f18"), "416459422322", Guid.Parse("afbd278c-5176-4510-9ad1-ae2acf34f80a"), Domain.Enums.AccountType.Checking, "USD"),
+                new Account(Guid.Parse("ee722d5c-fa75-49ff-a438-ae2ea86b2f18"), "416459422322", Guid.Parse("afbd278c-5176-4510-9ad1-ae2acf34f80a"), Domain.Enums.AccountType.Checking, "USD", Domain.Enums.AccountStatus.Active),
                 
-                new Account(Guid.Parse("1f6ec25e-6086-4747-b476-aabc217773ea"), "145967913707", Guid.Parse("afbd278c-5176-4510-9ad1-ae2acf34f80a"), Domain.Enums.AccountType.Savings, "USD"),
+                new Account(Guid.Parse("1f6ec25e-6086-4747-b476-aabc217773ea"), "145967913707", Guid.Parse("afbd278c-5176-4510-9ad1-ae2acf34f80a"), Domain.Enums.AccountType.Savings, "USD", Domain.Enums.AccountStatus.Active),
                 
-                new Account(Guid.Parse("05781c31-1bd7-46a6-8554-b0470866c62e"), "228947707340", Guid.Parse("418586f3-7268-4035-8ae7-d2241c474876"), Domain.Enums.AccountType.Checking, "USD"),
+                new Account(Guid.Parse("05781c31-1bd7-46a6-8554-b0470866c62e"), "228947707340", Guid.Parse("418586f3-7268-4035-8ae7-d2241c474876"), Domain.Enums.AccountType.Checking, "USD", Domain.Enums.AccountStatus.Active),
                 
-                new Account(Guid.Parse("e0b80df6-49a6-4c75-8b99-b184ad1c2fb7"), "681627673954", Guid.Parse("418586f3-7268-4035-8ae7-d2241c474876"), Domain.Enums.AccountType.Savings, "USD")
+                new Account(Guid.Parse("e0b80df6-49a6-4c75-8b99-b184ad1c2fb7"), "681627673954", Guid.Parse("418586f3-7268-4035-8ae7-d2241c474876"), Domain.Enums.AccountType.Savings, "USD", Domain.Enums.AccountStatus.Active)
             );
         }
     }
--- BankingApp.Infrastructure/Persistence/Configurations/CustomerConfiguration.cs
+++ BankingApp.Infrastructure/Persistence/Configurations/CustomerConfiguration.cs
@@ -13,12 +13,18 @@ namespace BankingApp.Infrastructure.Persistence.Configurations
         public void Configure(EntityTypeBuilder<Customer> builder)
         {
             builder.HasKey(c => c.Id);
+
             builder.Property(c => c.FirstName).IsRequired().HasMaxLength(25);
             builder.Property(c => c.LastName).IsRequired().HasMaxLength(25);
             builder.Property(c => c.DateOfBirth).IsRequired().HasColumnType("datetimeoffset");
+            builder.Property(c => c.CustomerStatus).IsRequired().HasDefaultValue(Domain.Enums.CustomerStatus.Active);
             builder.Property(c => c.PhoneNumber).IsRequired().HasMaxLength(15);
             builder.Property(c => c.Email).IsRequired().HasMaxLength(50);
-            builder.HasMany<Account>().WithOne().HasForeignKey(a => a.CustomerId);
+
+            builder.HasMany(c => c.Accounts)
+                   .WithOne()
+                   .HasForeignKey(a => a.CustomerId);
+
             builder.HasData(
                 new Customer(Guid.Parse("418586f3-7268-4035-8ae7-d2241c474876"), "Omar", "Williams", "omarwilliams@gmail.com", "111-111-1111", DateTimeOffset.Parse("10/20/1997")),
                 new Customer(Guid.Parse("4bf2a680-ac31-4206-be1a-8effc3d6c427"), "Harlem", "Williams", "harlemwilliams@gmail.com", "222-222-2222", DateTimeOffset.Parse("1/2/2010")),
